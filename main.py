1c4d7bb2fadfb08dbd1ed605e99d12d75ddb8393
Lieffring Florentin <lieffring.florentin@gmail.com>
2017-12-12 00:07:58 +0100

    work in progress

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import subprocess
 import time
 from tkinter import *
 from tkinter.messagebox import *
+import threading
 
 import requests
 
@@ -14,59 +15,81 @@ applicationName = "openfortivpn"
 root = Tk()
 root.title(applicationName)
 #root.geometry(WINDOW_SIZE)
+#root.configure(background='#666666')
+root.minsize(width=700,height=650)
+root.maxsize(width=700,height=650)
 
 #Défition des fonctions
 def vpnConnect():
-    p = subprocess.Popen(['openfortivpn',
-                          vpngateway.get() + ":" + vpngatewayport.get(),
-                          '--username='+username.get(),
-                          '--trusted-cert',
-                          vpngatewaycert.get()
-                          ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-    p.stdin.write((password.get()).encode())
+    def callback():
+        pConnect = subprocess.Popen(['openfortivpn',
+                              vpngateway.get() + ":" + vpngatewayport.get(),
+                              '--username='+username.get(),
+                              '--trusted-cert',
+                              vpngatewaycert.get()
+                              ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        pConnect.stdin.write((password.get()).encode())
 
-    output = p.communicate()[0]
+        output = pConnect.communicate()
 
-    shellOutput.delete('1.0', END)
-    shellOutput.insert(END, output)
-    #showwarning(applicationName, output)
+        shellOutput.delete('1.0', END)
+        shellOutput.insert(END, output)
+        #showwarning(applicationName, output)
+
+    t = threading.Thread(target=callback)
+    t.start()
+
+def vpnDisConnect():
+    def callback():
+        findProcessId = subprocess.Popen(['pgrep', 'openfortivpn'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        processId = findProcessId.stdout.read()
+        p = subprocess.Popen(['kill', processId])
+
+        showwarning(applicationName, processId)
+
+    t = threading.Thread(target=callback)
+    t.start()
 
 def callbackQuitWindow():
-    showwarning(applicationName, 'The application will now kill the VPN tunnel')
+    #showwarning(applicationName, 'The application will now kill the VPN tunnel')
+    #vpnDisConnect()
     root.quit()
 
 #Template
 response = requests.get('https://httpbin.org/ip')
 Label(root, text='Votre IP: ' + format(response.json()['origin'])).grid(row=0, column=0)
-Label(root, text='Heure de lancement ' + time.strftime("%I:%M:%S") + '\n').grid(row=0, column=1)
+Label(root, text='Heure de lancement ' + time.strftime("%I:%M:%S")).grid(row=0, column=1)
 
 Label(root, text='Username').grid(row=1,column=0)
-username = Entry(root)
+username = Entry(root, width=22)
 username.grid(row=1,column=1)
 
 Label(root, text='Password').grid(row=2,column=0)
-password = Entry(root)
+password = Entry(root, width=22)
 password.grid(row=2,column=1)
 
 Label(root, text='Gateway').grid(row=3,column=0)
-vpngateway = Entry(root)
+vpngateway = Entry(root, width=22)
 vpngateway.grid(row=3,column=1)
 
 Label(root, text='Port').grid(row=4,column=0)
-vpngatewayport = Entry(root)
+vpngatewayport = Entry(root, width=22)
 vpngatewayport.grid(row=4,column=1)
 
-Label(root, text='Gateway Cert').grid(row=5,column=0)
-vpngatewaycert = Entry(root)
+Label(root, text='Gateway Trusted Cert').grid(row=5,column=0)
+vpngatewaycert = Entry(root, width=22)
 vpngatewaycert.grid(row=5,column=1)
 
-Button(root, text="Connect", command=vpnConnect).grid(row=6,column=2)
+connectButton = Button(root, text="Connect", command=vpnConnect, height=1, width=19)
+connectButton.grid(row=7,column=1)
 
 shellOutput = Text(root)
 shellOutput.config(width=99, height=25)
-shellOutput.grid(row=99,column=0, columnspan=3)
+shellOutput.configure(background='#333333', foreground='#dedede')
+shellOutput.grid(row=99,column=0, columnspan=2)
 shellOutput.insert(END, 'Ready to connect')
 
-Button(root, text='Quit', command=callbackQuitWindow).grid(row=100,column=2)
+quitButton = Button(root, text='Quit', command=callbackQuitWindow, height=2, width=20)
+quitButton.grid(row=110,column=1,padx=0, pady=0)
 
 root.mainloop()
