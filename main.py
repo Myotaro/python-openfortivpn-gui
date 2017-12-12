e7ccb3f7e3832aa85221b97f8c6e9d51bcb85a31
Lieffring Florentin <lieffring.florentin@gmail.com>
2017-12-12 20:42:03 +0100

    Cleaned the code.

--- main.py
+++ main.py
@@ -1,45 +1,28 @@
-#!../../.local/share/virtualenvs/LearnPython-UdQ-JvbQ/bin/python3
+#!/usr/bin/python3
 
 import subprocess
-import time
-from tkinter import *
-from tkinter.messagebox import *
 import threading
-import requests
 import os
 import signal
+from tkinter import *
+from tkinter.messagebox import *
 
-#Défition des variables fixes
 applicationName = "openfortivpn"
-#WINDOW_SIZE = "800x800"
 
 root = Tk()
 root.title(applicationName)
-#root.geometry(WINDOW_SIZE)
-#root.configure(background='#666666')
-root.minsize(width=700,height=650)
-root.maxsize(width=700,height=650)
 
-#Défition des fonctions
-def bytes_to_int(bytes):
-    result = 0
-
-    for b in bytes:
-        result = result * 256 + int(b)
-
-    return result
-
+# Fonctions
 def vpnConnect():
     findProcessId = subprocess.Popen(['pgrep', 'openfortivpn'], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                      stdin=subprocess.PIPE)
     processId = findProcessId.stdout.read()
-    #processIdInt = int(str(processId, 'utf-8'))
     if processId:
         showwarning(applicationName, 'Déjà connecté!!')
     else:
         def callback():
             pConnect = subprocess.Popen(['openfortivpn',
-                                  vpngateway.get() + ":" + vpngatewayport.get(),
+                                  vpngateway.get(),
                                   '--username='+username.get(),
                                   '--trusted-cert',
                                   vpngatewaycert.get()
@@ -50,7 +33,6 @@ def vpnConnect():
 
             shellOutput.delete('1.0', END)
             shellOutput.insert(END, output)
-            #showwarning(applicationName, output)
 
         t = threading.Thread(target=callback)
         t.start()
@@ -61,48 +43,41 @@ def vpnDisConnect():
     if processId:
         processIdInt = int(str(processId,'utf-8'))
         if processIdInt > 0:
-            #showwarning(applicationName, processIdInt)
-            os.kill(processIdInt, signal.SIGKILL)  # signal.SIGTERM or signal.SIGKILL
+            os.kill(processIdInt, signal.SIGKILL)
 
 def callbackQuitWindow():
     vpnDisConnect()
     root.quit()
 
-#Template
-response = requests.get('https://httpbin.org/ip')
-Label(root, text='Votre IP: ' + format(response.json()['origin'])).grid(row=0, column=0)
-Label(root, text='Heure de lancement ' + time.strftime("%I:%M:%S")).grid(row=0, column=1)
+# GUI
+Label(root, text='Gateway : Port').grid(row=1,column=0, sticky="w")
+vpngateway = Entry(root, width=45)
+vpngateway.grid(row=1,column=1,padx=0, pady=5)
 
-Label(root, text='Username').grid(row=1,column=0)
-username = Entry(root, width=22)
-username.grid(row=1,column=1)
+Label(root, text='Username').grid(row=2,column=0, sticky="w")
+username = Entry(root, width=45)
+username.grid(row=2,column=1,padx=0, pady=5)
 
-Label(root, text='Password').grid(row=2,column=0)
-password = Entry(root, width=22)
-password.grid(row=2,column=1)
+Label(root, text='Password').grid(row=3,column=0, sticky="w")
+password = Entry(root, width=45)
+password.grid(row=3,column=1,padx=0, pady=5)
 
-Label(root, text='Gateway').grid(row=3,column=0)
-vpngateway = Entry(root, width=22)
-vpngateway.grid(row=3,column=1)
-
-Label(root, text='Port').grid(row=4,column=0)
-vpngatewayport = Entry(root, width=22)
-vpngatewayport.grid(row=4,column=1)
-
-Label(root, text='Gateway Trusted Cert').grid(row=5,column=0)
-vpngatewaycert = Entry(root, width=22)
-vpngatewaycert.grid(row=5,column=1)
-
-connectButton = Button(root, text="Connect", command=vpnConnect, height=1, width=19)
-connectButton.grid(row=7,column=1)
+Label(root, text='Gateway Trusted Cert').grid(row=4,column=0, sticky="w")
+vpngatewaycert = Entry(root, width=45)
+vpngatewaycert.grid(row=4,column=1,padx=0, pady=5)
 
 shellOutput = Text(root)
-shellOutput.config(width=99, height=25)
+shellOutput.config(width=79, height=15)
 shellOutput.configure(background='#333333', foreground='#dedede')
-shellOutput.grid(row=99,column=0, columnspan=2)
-shellOutput.insert(END, 'Ready to connect')
+shellOutput.grid(row=99,column=0, columnspan=5)
+shellOutput.insert(END, '')
+
+connectButton = Button(root, text="Connect", command=vpnConnect, height=2, width=20)
+connectButton.configure(background='green', foreground='#dedede', border="2px")
+connectButton.grid(row=110,column=0)
 
 quitButton = Button(root, text='Quit', command=callbackQuitWindow, height=2, width=20)
-quitButton.grid(row=110,column=1,padx=0, pady=0)
+quitButton.configure(background='red', foreground='#dedede', border="2px")
+quitButton.grid(row=110,column=1,padx=0, pady=0, sticky="e")
 
 root.mainloop()
