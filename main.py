58c52811b764fecf3896b6fa0fb5e4e64238507a
Lieffring Florentin <lieffring.florentin@gmail.com>
2017-12-13 21:28:41 +0100

    Improvements to UI's general behavior

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ root = Tk()
 root.title(applicationName)
 
 # Fonctions
+def findTrustedCert( s, first, last ):
+    try:
+        start = s.rindex( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
 def vpnConnect():
     findProcessId = subprocess.Popen(['pgrep', 'openfortivpn'], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                      stdin=subprocess.PIPE)
@@ -21,17 +29,24 @@ def vpnConnect():
         showwarning(applicationName, 'You are already connected')
     else:
         def callback():
-            pConnect = subprocess.Popen(['openfortivpn',
-                                  vpngateway.get(),
-                                  '--username='+username.get(),
-                                  '--trusted-cert',
-                                  vpngatewaycert.get()
-                                  ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-            pConnect.stdin.write((password.get()).encode())
+            def askAccess():
+                pConnect = subprocess.Popen(['openfortivpn',
+                                      vpngateway.get(),
+                                      '--username='+username.get(),
+                                      '--trusted-cert',
+                                      vpngatewaycert.cget('text')
+                                      ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                pConnect.stdin.write((password.get()).encode())
 
-            output = pConnect.communicate()
-            showwarning(applicationName, output)
-            showwarning(applicationName, 'Error')
+                output = pConnect.communicate()
+                outputCert = findTrustedCert(str(output),'--trusted-cert ', '\\n' )
+                vpngatewaycert.config(text=outputCert)
+                if outputCert:
+                    showwarning(applicationName, 'Connected')
+                    askAccess()  # Connect
+                else:
+                    showwarning(applicationName, 'Please verify your credentials')
+            askAccess() # Get Cert string
 
         t = threading.Thread(target=callback)
         t.start()
@@ -59,7 +74,7 @@ password = Entry(root, show="*", width=45)
 password.grid(row=3,column=1,padx=0, pady=5)
 
 Label(root, text='Gateway Trusted Cert').grid(row=4,column=0, sticky="w")
-vpngatewaycert = Entry(root, width=45)
+vpngatewaycert = Label(root, text=' ', width=45)
 vpngatewaycert.grid(row=4,column=1,padx=0, pady=5)
 
 connectButton = Button(root, text="Connect", command=vpnConnect, height=2, width=20)
@@ -70,4 +85,5 @@ quitButton = Button(root, text='Quit', command=vpnDisConnect, height=2, width=20
 quitButton.configure(background='red', foreground='#dedede', border="2px")
 quitButton.grid(row=110,column=1,padx=0, pady=0, sticky="e")
 
+root.protocol("WM_DELETE_WINDOW", vpnDisConnect)
 root.mainloop()
