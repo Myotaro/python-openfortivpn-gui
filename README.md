509491f8f9f5eba1a410c0e59b70ed0cb58f87d3
Lieffring Florentin <lieffring.florentin@gmail.com>
2017-12-13 22:53:07 +0100

    Improvements to UI, cleaned some code, updated readme.md with screenshot and installation process

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # python-openfortivpn-gui
+
+![Screenshot](screenshot.png)
+
 GUI in development for the openfortivpn writen in Python3
 
 Github page of openfortivpn from Adrien Vergé: https://github.com/adrienverge/openfortivpn
 
 1) Install openfortivpn
-2) sudo ./main.py
+2) git clone https://github.com/Myotaro/python-openfortivpn-gui.git
+3) cd python-openfortivpn-gui
+4) sudo ./main.py
 
-This is my first Python3 project. 
+First Python3 project.
 Feedbacks are of course very welcome.
--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import signal
 from tkinter import *
 from tkinter.messagebox import *
 
-applicationName = "openfortivpn"
+applicationName = "python-openfortivpn-gui"
 
 root = Tk()
 root.title(applicationName)
+root.resizable(width=False, height=False)
+root.minsize(width=650, height=25)
 
 # Fonctions
 def findTrustedCert( s, first, last ):
@@ -21,10 +23,14 @@ def findTrustedCert( s, first, last ):
     except ValueError:
         return ""
 
-def vpnConnect():
-    findProcessId = subprocess.Popen(['pgrep', 'openfortivpn'], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                     stdin=subprocess.PIPE)
+def getProcessId():
+    findProcessId = subprocess.Popen(['pgrep', 'openfortivpn'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     processId = findProcessId.stdout.read()
+    return (processId)
+
+def vpnConnect():
+    processId = getProcessId()
+
     if processId:
         showwarning(applicationName, 'You are already connected')
     else:
@@ -36,54 +42,64 @@ def vpnConnect():
                                       '--trusted-cert',
                                       vpngatewaycert.cget('text')
                                       ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
                 pConnect.stdin.write((password.get()).encode())
 
                 output = pConnect.communicate()
                 outputCert = findTrustedCert(str(output),'--trusted-cert ', '\\n' )
                 vpngatewaycert.config(text=outputCert)
+
                 if outputCert:
                     showwarning(applicationName, 'Connected')
                     askAccess()  # Connect
                 else:
                     showwarning(applicationName, 'Please verify your credentials')
+
             askAccess() # Get Cert string
 
         t = threading.Thread(target=callback)
         t.start()
 
-def vpnDisConnect():
-    findProcessId = subprocess.Popen(['pgrep', 'openfortivpn'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-    processId = findProcessId.stdout.read()
+def vpnDisconnect():
+    processId = getProcessId()
+
     if processId:
         processIdInt = int(str(processId,'utf-8'))
         if processIdInt > 0:
+            showwarning(applicationName, 'Disconnected')
             os.kill(processIdInt, signal.SIGKILL)
     root.quit()
 
 # GUI
-Label(root, text='Gateway : Port').grid(row=1,column=0, sticky="w")
+banner = PhotoImage(file="logo.png")
+
+canvas = Canvas(root, width=650, height=50)
+canvas.create_image(0, 0, anchor=NW, image=banner)
+canvas.grid(row=1,column=0, columnspan=2, pady=15)
+
+Label(root, text='Gateway : Port').grid(row=10,column=0, sticky="w", padx=25, pady=5)
 vpngateway = Entry(root, width=45)
-vpngateway.grid(row=1,column=1,padx=0, pady=5)
+vpngateway.grid(row=10,column=1,padx=5, pady=5)
 
-Label(root, text='Username').grid(row=2,column=0, sticky="w")
+Label(root, text='Username').grid(row=20,column=0, sticky="w", padx=25, pady=5)
 username = Entry(root, width=45)
-username.grid(row=2,column=1,padx=0, pady=5)
+username.grid(row=20,column=1,padx=0, pady=5)
 
-Label(root, text='Password').grid(row=3,column=0, sticky="w")
+Label(root, text='Password').grid(row=30,column=0, sticky="w", padx=25, pady=5)
 password = Entry(root, show="*", width=45)
-password.grid(row=3,column=1,padx=0, pady=5)
+password.grid(row=30,column=1,padx=0, pady=5)
 
-Label(root, text='Gateway Trusted Cert').grid(row=4,column=0, sticky="w")
+Label(root, text='Gateway Trusted Cert').grid(row=40,column=0, sticky="w", padx=25, pady=5)
 vpngatewaycert = Label(root, text=' ', width=45)
-vpngatewaycert.grid(row=4,column=1,padx=0, pady=5)
+vpngatewaycert.grid(row=40,column=1,padx=0, pady=5)
 
 connectButton = Button(root, text="Connect", command=vpnConnect, height=2, width=20)
-connectButton.configure(background='green', foreground='#dedede', border="2px")
-connectButton.grid(row=110,column=0)
+connectButton.configure(background='#63b044', foreground='#dedede', border="0")
+connectButton.grid(row=100,column=0, pady=25)
 
-quitButton = Button(root, text='Quit', command=vpnDisConnect, height=2, width=20)
-quitButton.configure(background='red', foreground='#dedede', border="2px")
-quitButton.grid(row=110,column=1,padx=0, pady=0, sticky="e")
+quitButton = Button(root, text='Quit', command=vpnDisconnect, height=2, width=20)
+quitButton.configure(background='#cb4141', foreground='#dedede', border="0")
+quitButton.grid(row=100,column=1, padx=25, pady=25, sticky="e")
 
-root.protocol("WM_DELETE_WINDOW", vpnDisConnect)
+root.protocol("WM_DELETE_WINDOW", vpnDisconnect)
 root.mainloop()
